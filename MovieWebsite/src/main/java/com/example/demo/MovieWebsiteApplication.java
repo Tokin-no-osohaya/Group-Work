ebf32a34a85878425eea4578596f1683585d79ad
小林 <3030963@30309-63>
2023-12-04 10:04:31 +0900

    更新

--- MovieWebsite/src/main/java/com/example/demo/MovieWebsiteApplication.java
+++ MovieWebsite/src/main/java/com/example/demo/MovieWebsiteApplication.java
@@ -1,42 +1,11 @@
 package com.example.demo;
 
-<<<<<<< HEAD
-=======
-//import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> branch 'master' of https://github.com/Tokin-no-osohaya/Group-Work.git
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-=======
-
-//import com.example.demo.entity.Movie;
->>>>>>> branch 'master' of https://github.com/Tokin-no-osohaya/Group-Work.git
-//import com.example.demo.repository.MovieRepository;
 
 @SpringBootApplication
 public class MovieWebsiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieWebsiteApplication.class, args);
-<<<<<<< HEAD
-=======
-//		.getBean(MovieWebsiteApplication.class).execute();
-//	}
-//	@Autowired
-//	MovieRepository repository;
-//	
-//	//実行メソッド
-//	private void execute() {
-//		//全件表示
-//		//showList();
-//	}
-//	
-//	//全件取得
-//	private void showList(){
-//		//リポジトリを使用して1件を表示（戻り値はOptional）
-//		Iterable<Movie> movies = repository.findAll();
-//		for(Movie movie : movies) {
-//			
-//		}
->>>>>>> branch 'master' of https://github.com/Tokin-no-osohaya/Group-Work.git
 	}
 }
--- MovieWebsite/src/main/java/com/example/demo/entity/Movie.java
+++ MovieWebsite/src/main/java/com/example/demo/entity/Movie.java
@@ -1,13 +1,7 @@
 package com.example.demo.entity;
 
-
-<<<<<<< HEAD
-//import org.springframework.data.annotation.Id;
-
-=======
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
->>>>>>> branch 'master' of https://github.com/Tokin-no-osohaya/Group-Work.git
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
